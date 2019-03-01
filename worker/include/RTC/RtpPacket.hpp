c1a0afc7256030fa2984bcae566a227ec16e9352
Jeremiah Seeley <jeremiah@livelyvideo.tv>
2019-03-01 16:38:30 -0600

    orientation; fix typo in ReadRid func

--- worker/include/RTC/RtpPacket.hpp
+++ worker/include/RTC/RtpPacket.hpp
@@ -119,7 +119,7 @@ namespace RTC
 		bool ReadAbsSendTime(uint32_t* time) const;
 		bool ReadVideoOrientation(const uint8_t** data, size_t* len) const;
 		bool ReadMid(const uint8_t** data, size_t* len) const;
-		bool eadRidR(const uint8_t** data, size_t* len) const;
+		bool ReadRid(const uint8_t** data, size_t* len) const;
 		uint8_t* GetPayload() const;
 		size_t GetPayloadLength() const;
 		uint8_t GetPayloadPadding() const;
@@ -343,7 +343,7 @@ namespace RTC
 		return true;
 	}
 
-	inline bool RtpPacket::ReadVideoOrientation(const uint8_t** rotation, size_t* len) const
+	inline bool RtpPacket::ReadVideoOrientation(const uint8_t** rotation) const
 	{
 		uint8_t extenLen;
 		uint8_t* extenValue;
