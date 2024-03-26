01678d639e296b7873224425c6fa04562235ad58
amirp <amirp@accretivetg.com>
2024-03-26 06:58:43 -0500

    fix: exception is thrown in ShmTransport::RecvStreamMeta because the shm is an object rather than a string

--- node/src/ShmTransport.ts
+++ node/src/ShmTransport.ts
@@ -114,9 +114,10 @@ export type ShmTransportObserverEvents = TransportObserverEvents;
 
 export type ShmTransportData = 
 {
-	name?: string;
-
-	log?: string;
+	shm: {
+		name?: string;
+		log?: string;
+	}
 };
 
 type ShmTransportConstructorOptions<ShmTransportAppData> =
@@ -146,11 +147,7 @@ export class ShmTransport<ShmTransportAppData extends AppData = AppData>
 
 		const { data } = options;
 
-		this.#data =
-		{
-			name : data.name,
-			log: data.log
-		}
+		this.#data = data;
 	}
 
 
@@ -364,8 +361,8 @@ export class ShmTransport<ShmTransportAppData extends AppData = AppData>
 
 		const reqData = {
 			meta,
-			shm: this.#data.name,
-			log: this.#data.log,
+			shm: this.#data.shm.name,
+			log: this.#data.shm.log,
 		};
 
 		await this.channel.request('transport.consumeStreamMeta', this.internal.transportId, reqData);
